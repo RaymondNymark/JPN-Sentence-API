a82f9cad3a974c74fe61037dce7e5fb4e741a5fc
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-02-19 13:42:09 +0100

    Extract interface of RetrieveSentene for abstraction

---
+++ JPN-SentenceAPI.Core/Services/SearchingService/IRetrieveSentence.cs
@@ -0,0 +1,12 @@
+﻿using JPN_SentenceAPI.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JPN_SentenceAPI.Core.Services
+{
+    public interface IRetrieveSentence
+    {
+        List<Sentence> RetrieveSentencesBySearchTerm(string searchTerm);
+        Task<Sentence> RetrieveSentencesBySearchTermAsync(string searchTerm);
+    }
+}
