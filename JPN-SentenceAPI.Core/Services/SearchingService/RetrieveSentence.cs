ef107d2063bdd9f6e12bb165a5da99c748a241f3
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-02-19 13:41:26 +0100

    Create RetrieveSentence.cs

---
+++ JPN-SentenceAPI.Core/Services/SearchingService/RetrieveSentence.cs
@@ -0,0 +1,20 @@
+﻿using JPN_SentenceAPI.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JPN_SentenceAPI.Core.Services
+{
+    public class RetrieveSentence : IRetrieveSentence
+    {
+        public List<Sentence> RetrieveSentencesBySearchTerm(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Sentence> RetrieveSentencesBySearchTermAsync(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
