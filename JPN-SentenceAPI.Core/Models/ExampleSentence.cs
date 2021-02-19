86c25813d171d4d304c60be448a5f2686526ccb4
Raymond Nymark <67239758+RaymondNymark@users.noreply.github.com>
2021-02-19 13:36:06 +0100

    Create ExampleSentence Model and create the parameters for it
    Each parameter is decorated with /// comments in order to fit with auto-documentation of API calls.

---
+++ JPN-SentenceAPI.Core/Models/ExampleSentence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace JPN_SentenceAPI.Core.Models
+{
+    /// <summary>
+    /// Represents one specific example sentence.
+    /// </summary>
+    public class Sentence
+    {
+        /// <summary>
+        /// Unique identifier, SQL ID
+        /// </summary>
+        [Required]
+        public int SentenceID { get; set; }
+
+        /// <summary>
+        /// The sentence's definition in Japanese.
+        /// </summary>
+        public string Japanese { get; set; }
+
+        /// <summary>
+        /// The sentence's definition in English. 
+        /// </summary>
+        public string English { get; set; }
+    }
+}
