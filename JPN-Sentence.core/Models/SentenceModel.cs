bad178481b99031bbe86d613b0e94d22aef91b31
Raymond Nymark <Ray.Nymark@gmail.com>
2021-08-27 13:27:18 +0200

    Create SentenceModel.cs

---
+++ JPN-Sentence.core/Models/SentenceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPN_Sentence.core.Models
+{
+    public class SentenceModel
+    {
+        public int ID { get; set; }
+        public string EnglishSentence { get; set; }
+        public string JapaneseSentence { get; set; }
+    }
+}
